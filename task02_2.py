78b817afdd2b4bea69ed533d29fe477746fc43dd
zprtal <tuzgleb75@gmail.com>
2026-04-13 22:07:14 +0300

    checked the solution to make sure it was correct. corrected the inaccuracies I've identified.

--- task02_2.py
+++ task02_2.py
@@ -1,8 +1,8 @@
 def isPolinom(x):
     s = str(x)
     for i in range(len(s)//2+1):
-        if s[i]!=s[-1 -i]:
-
+        c = s[i]!=s[-1 - i]
+        if s[i]!=s[-1 - i]:
             return False
     return True
 
@@ -12,28 +12,23 @@ def longestCommonPrefix(strs):
     for i in range(len(strs[0])):
         s = s + strs[0][i]
         for word in strs:
-           if word[i]!=s[-1]:
-               s = s[:-1]
-               return s
+            if i >= len(word) or word[i] != s[-1]:
+                s = s[:-1]
+                return s
     return s
 
-def removeDuplicates(nums):
-    digit=-101
-    new_nums =[]
-    size = len(nums)
-    for i in range(len(nums)):
 
-        if digit != nums[i]:
-            digit = nums[i]
-            new_nums.append(digit)
-        else:
-            size -= 1
-            nums[i] = '_'
-    new_nums.extend('_' * (len(nums) - size))
-    return size, new_nums
+def removeDuplicates(nums):
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[k - 1]:
+            nums[k] = nums[i]
+            k += 1
+    return k
 
 
 def twoSum(nums, target):
+    nums.sort()
     for i in range(len(nums)):
         for k in range(i+1, len(nums)):
 
@@ -41,8 +36,8 @@ def twoSum(nums, target):
                 return [i,k]
 
 def addBinary( a, b):
-    rezult = bin((int(a, 2))+ (int(b, 2)))
-    return rezult [2:]
+    result = bin((int(a, 2))+ (int(b, 2)))
+    return result [2:]
 
 
 #print(isPolinom(121))                                          #true
@@ -51,10 +46,10 @@ def addBinary( a, b):
 
 #print(longestCommonPrefix(["flower","flow","flight"]))         #fl
 #print(longestCommonPrefix(["dog","racecar","car"]))            #""
+#print(longestCommonPrefix(["dog","racecar","car"]))            #""
+#print(longestCommonPrefix(["","d"]))            #""
 
-#size, my_list = removeDuplicates([1,1,2])                      #2, nums = [1,2,_]
-#size, my_list = removeDuplicates([0,0,1,1,1,2,2,3,3,4])        #5, nums = [0,1,2,3,4,_,_,_,_,_]
-#print(size, "nums = ", my_list)
+#removeDuplicates([1,1,2])                      #2, nums = [1,2,_]
 
 #print( twoSum([2,7,11,15], 9))                                 #[0,1]
 #print( twoSum([3,2,4], 6))                                     #[1,2]

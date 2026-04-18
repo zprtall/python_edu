e6e0a85c5db0a92aeb7ef8a1a41d1485b5a8093d
zprtal <tuzgleb75@gmail.com>
2026-04-18 19:05:26 +0300

    completed Task 1 in Task1.2

---
+++ 1.2Task_1.py
@@ -0,0 +1,26 @@
+class Point_X_Y:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __bool__(self,):
+        return self.x < 0 or self.y < 0
+
+    def __str__(self,):
+        return f"Точка имеет значеня -\nX: {self.x} \nY: {self.y}"
+
+    def __sub__(self, other):
+        return ((other.x - self.x) ** 2 + (other.y - self.y) ** 2) ** 0.5
+
+point1 = Point_X_Y(10, -5)
+point2 = Point_X_Y(15, 0)
+print(point1)
+print(point2)
+
+print(point1 - point2)
+
+if point1:
+    print("Есть отрицательная координата")
+else:
+    print("Обе координаты >= 0")

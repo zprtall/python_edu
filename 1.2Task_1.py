b3583717ad171c50a9d9586f0f1b19ac8bd83a1e
zprtal <tuzgleb75@gmail.com>
2026-04-22 18:58:10 +0300

    + class Figure + class Quadrilateral(Figure)

--- 1.2Task_1.py
+++ 1.2Task_1.py
@@ -13,7 +13,37 @@ class Point:
     def __sub__(self, other):
         return ((other.x - self.x) ** 2 + (other.y - self.y) ** 2) ** 0.5
 
-class Triangle(Point):
+class Figure:
+    def area(self):
+         pass
+
+    def perimetr(self):
+        pass
+
+class Quadrilateral(Figure):
+    def __init__(self, p1, p2, p3, p4):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+
+    def sides(self):
+        s1 = self.p1 - self.p2
+        s2 = self.p2 - self.p3
+        s3 = self.p3 - self.p4
+        s4 = self.p4 - self.p1
+        return s1, s2, s3, s4
+
+    def area(self):
+        p = sum(self.sides()) / 2
+        s1, s2, s3, s4 = self.sides()
+        return ((p - s1) * (p - s2) * (p - s3) * (p - s4)) **0.5
+
+    def perimetr(self):
+        return sum(self.sides())
+
+
+class Triangle:
     def __init__(self, p1, p2, p3):
         self.p1 = p1
         self.p2 = p2
@@ -54,7 +84,7 @@ class Triangle(Point):
         return s1 ** 2 + s2 ** 2 > s3 ** 2 or s2 ** 2 + s3 ** 2 > s1 ** 2 or s3 ** 2 + s1 ** 2 > s2 ** 2
 
     @property
-    def square(self):                # получение площади
+    def area(self):                # получение площади
         s1, s2, s3 = self.sides()
         p = (s1 + s2 + s3)/2
         return (p * (p - s1) * (p - s2) * (p - s3)) **0.5
@@ -63,5 +93,7 @@ triangle1 = Triangle(Point(1,2), Point(3,4), Point(-1, 8))
 print(triangle1.sides())
 
 print(triangle1.sharp_angled)
-print(triangle1.square)
+print(triangle1.area)
 print(triangle1)
+
+quadrilateral1 = Quadrilateral(Point(1,2), Point(3,4), Point(-1, 8), Point(4, 5))

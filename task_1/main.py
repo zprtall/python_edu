522e67d2ecb58de9460c98e3514738a7209a44b2
zprtal <tuzgleb75@gmail.com>
2026-05-11 23:51:06 +0300

    feat: create initial REST API structure

---
+++ task_1/main.py
@@ -0,0 +1,108 @@
+from fastapi import FastAPI, HTTPException
+
+from models import Struct, PatchValue
+from storage import db
+
+app = FastAPI()
+
+
+@app.post("/struct")
+def create_struct(struct: Struct):
+
+    new_id = 1
+
+    while new_id in db:
+        new_id += 1
+
+    db[new_id] = struct.dict()
+
+    return {
+        "message": "Struct created",
+        "id": new_id,
+        "data": db[new_id]
+    }
+
+
+@app.get("/struct/{struct_id}")
+def get_struct(struct_id: int):
+
+    if struct_id not in db:
+        raise HTTPException(
+            status_code=404,
+            detail="Struct not found"
+        )
+
+    return db[struct_id]
+
+
+@app.get("/struct")
+def get_all_structs():
+
+    if len(db) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail="No structures found"
+        )
+
+    return db
+
+
+@app.put("/struct/{struct_id}")
+def put_struct(struct_id: int, struct: Struct):
+
+    if struct_id not in db:
+        raise HTTPException(
+            status_code=404,
+            detail="Struct not found"
+        )
+
+    db[struct_id] = struct.dict()
+
+    return {
+        "message": "Structure updated",
+        "data": db[struct_id]
+    }
+
+
+@app.patch("/struct/{struct_id}/{struct_field}")
+def patch_struct(
+    struct_id: int,
+    struct_field: str,
+    patch: PatchValue
+):
+
+    if struct_id not in db:
+        raise HTTPException(
+            status_code=404,
+            detail="Struct not found"
+        )
+
+    if struct_field not in db[struct_id]:
+        raise HTTPException(
+            status_code=404,
+            detail="Field not found"
+        )
+
+    db[struct_id][struct_field] = patch.value
+
+    return {
+        "message": "Structure changed",
+        "data": db[struct_id]
+    }
+
+
+@app.delete("/struct/{struct_id}")
+def delete_struct(struct_id: int):
+
+    if struct_id not in db:
+        raise HTTPException(
+            status_code=404,
+            detail="Struct not found"
+        )
+
+    deleted = db.pop(struct_id)
+
+    return {
+        "message": "Structure deleted",
+        "data": deleted
+    }
---
+++ task_1/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+import datetime
+
+
+class Struct(BaseModel):
+    number_of_tooth: int
+    hight: float
+    name: str
+    created_at: datetime.date
+    birth_date: datetime.date
+
+
+class PatchValue(BaseModel):
+    value: str

1885e985b7bd0194b38c919f9ba81302d913611c
zprtal <tuzgleb75@gmail.com>
2026-05-29 00:17:28 +0300

    feat(settings): myapp feat(models): Company, Workshop, Worker, Order

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Company, Workshop, Worker, Order
+
+admin.site.register(Company)
+admin.site.register(Workshop)
+admin.site.register(Worker)
+admin.site.register(Order)
 
 # Register your models here.

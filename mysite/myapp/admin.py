de25957668030a9fe16b1ffd365cb1d1adfdf62b
zprtal <tuzgleb75@gmail.com>
2026-05-29 15:13:22 +0300

    fix(models): feat(admin): inline rename app

--- mysite/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Company, Workshop, Worker, Order
-
-admin.site.register(Company)
-admin.site.register(Workshop)
-admin.site.register(Worker)
-admin.site.register(Order)
-
-# Register your models here.
